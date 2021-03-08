5e6d5d900a08e3fd3c191228d316381c2e33e133
randomicecube <diogo.marques.gaspar@tecnico.ulisboa.pt>
2021-03-08 14:39:04 +0000

    [LP] Aula 3 - Lógica Proposicional aplicada a sistemas computacionais

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -62,7 +62,7 @@ module.exports = {
         {
           title: '📝 Conteúdo',
           collapsable: false,
-          children: ['0001-conceitos-gerais', '0002-logica-proposicional'],
+          children: ['0001-conceitos-gerais', '0002-logica-proposicional-int', '0003-logica-proposicional-sc'],
         },
       ],
       '/cdi-ii/': [
