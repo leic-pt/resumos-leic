83c16d55bada9107b947ee4ff58281bcc6438399
Diogo Correia <me@diogotc.com>
2021-03-18 13:14:27 +0000

    [IAED] Add lab04 solutions
    Signed-off-by: Diogo Correia <me@diogotc.com>

---
+++ src/iaed/labs/lab04/ex05_dc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define MAX 80
+
+int leLinha(char s[]);
+
+int main()
+{
+  char s[MAX];
+  int i, size;
+
+  size = leLinha(s);
+
+  for (i = 0; i < size; ++i)
+    putchar(s[i]);
+  putchar('\n');
+
+  return 0;
+}
+
+int leLinha(char s[])
+{
+  int i = 0;
+  char c;
+  while ((c = getchar()) != EOF && c != '\n' && i < MAX - 1)
+    s[i++] = c;
+  s[i] = '\0';
+  return i;
+}
