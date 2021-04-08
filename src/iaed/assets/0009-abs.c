5a0b8c2251c3df68fd9b471720ad9db9d97e6acb
Tomás Esteves <69385653+Pesteves2002@users.noreply.github.com>
2021-04-08 18:20:46 +0100

    [IAED] Aula 8 & 9 (#96)

---
+++ src/iaed/assets/0009-abs.c
@@ -0,0 +1,12 @@
+typedef int Item; /*ignorem as barras \ */
+#define key(A) (A)
+#define less(A, B) (key(A) < key(B))
+#define exch(A, B)  \
+    {               \
+        Item t = A; \
+        A = B;      \
+        B = t;      \
+    }
+#define compexch(A, B) \
+    if (less(B, A))    \
+    exch(A, B)
---
+++ src/iaed/assets/0009-bubble_sort.c
@@ -0,0 +1,7 @@
+void bubble(Item a[], int left, int right)
+{
+    int i, j;
+    for (i = left; i < right; i++)
+        for (j = left; j < right + (left - i); j++)
+            compexch(a[j], a[j + 1]);
+}
