3072a7d3eeb410936fbd40c6405f4851e4a6f2af
Tomás Esteves <tomasesteves2002@gmail.com>
2021-04-15 19:52:07 +0100

    [IAED] Add new algos to cheat sheet

---
+++ src/iaed/assets/1000-quick.c
@@ -0,0 +1,32 @@
+void quicksort(Item a[], int left, int right)
+{
+    int i;
+    int j;
+    if (right <= left)
+        return;
+
+    i = partition(a, left, right);
+
+    quicksort(a, left, i - 1);
+    quicksort(a, i + 1, right);
+}
+
+int partition(Item a[], int left, int right)
+{
+    int i = left - 1;
+    int j = right;
+    Item v = a[right];
+    while (i < j)
+    {
+        while (less(a[++i], v))
+            ;
+        while (less(v, a[--j]))
+            if (j == left)
+                break;
+        if (i < j)
+            exch(a[i], a[j]);
+    }
+    exch(a[i], a[right]);
+
+    return i;
+}
