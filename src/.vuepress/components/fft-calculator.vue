2f94a0497355041e5d9f09275affcfed37e10469
Diogo Correia <me@diogotc.com>
2021-04-27 12:04:44 +0100

    Finish calculator logic

--- src/.vuepress/components/fft-calculator.vue
+++ src/.vuepress/components/fft-calculator.vue
@@ -50,6 +50,43 @@ export default {
       return `${real} ${imag > 0 ? '+' : ''}${imag}i`;
     },
 
+    mulComplex(c1, c2) {
+      // (a + bi) * (c + di)
+      // ac + adi + bci - bd
+      return {
+        real: c1.real * c2.real - c1.imag * c2.imag,
+        imag: c1.real * c2.imag + c1.imag * c2.real,
+      };
+    },
+
+    sumComplex(c1, c2) {
+      return { real: c1.real + c2.real, imag: c1.imag + c2.imag };
+    },
+
+    complementComplex(c) {
+      return { real: c.real, imag: -c.imag };
+    },
+
+    multiplyStep1([v1, v2]) {
+      const minusOne = { real: -1, imag: 0 };
+      return [this.sumComplex(v1, v2), this.sumComplex(v1, this.mulComplex(minusOne, v2))];
+    },
+
+    multiplyStep2([v1, v2]) {
+      const imagOne = { real: 0, imag: 1 };
+      return [v1, this.mulComplex(imagOne, v2)];
+    },
+
+    sumStep3([v1, v2], [v3, v4]) {
+      const minusOne = { real: -1, imag: 0 };
+      return [
+        this.sumComplex(v1, v3),
+        this.sumComplex(v2, v4),
+        this.sumComplex(v1, this.mulComplex(minusOne, v3)),
+        this.sumComplex(v2, this.mulComplex(minusOne, v4)),
+      ];
+    },
+
     calculate() {
       this.expressions = [];
 
@@ -64,14 +101,22 @@ export default {
         \\text{Original:} & [${pol1
           .map((v) => this.complexToStr(v))
           .join(', ')}] & [${pol2.map((v) => this.complexToStr(v)).join(', ')}]\\\\
-        \\text{Invertido:} & [${pol1Rev
+        \\text{Invertido } Rev_2: & [${pol1Rev
           .map((v) => this.complexToStr(v))
           .join(', ')}] & [${pol2Rev.map((v) => this.complexToStr(v)).join(', ')}]
         \\end{array}
       `;
       this.expressions.push(rev2);
 
-      const genF = (pol) => `
+      const genF = (pol) => {
+        const a1 = this.multiplyStep1(pol.slice(0, 2));
+        const a2 = this.multiplyStep1(pol.slice(2));
+        const a3 = this.multiplyStep2(a2);
+        const a4 = this.sumStep3(a1, a3);
+
+        return {
+          result: a4,
+          text: `
         \\begin{aligned}
         \\mathcal{F}_2(i) \\begin{pmatrix}${pol
           .map((v) => this.complexToStr(v))
@@ -125,10 +170,96 @@ export default {
               .map((v) => this.complexToStr(v))
               .join('\\\\')}
             \\end{pmatrix}
+          \\end{pmatrix}\\\\
+          &=\\begin{pmatrix}
+            \\begin{pmatrix}
+              ${a1.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+            +
+            \\begin{pmatrix}
+              1 & 0 \\\\ 0 & i
+            \\end{pmatrix}
+            \\begin{pmatrix}
+              ${a2.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+            \\\\
+            \\begin{pmatrix}
+              ${a1.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+            -
+            \\begin{pmatrix}
+              1 & 0 \\\\ 0 & i
+            \\end{pmatrix}
+            \\begin{pmatrix}
+              ${a2.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+          \\end{pmatrix}\\\\
+          &=\\begin{pmatrix}
+            \\begin{pmatrix}
+              ${a1.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+            +
+            \\begin{pmatrix}
+              ${a3.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+            \\\\
+            \\begin{pmatrix}
+              ${a1.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+            -
+            \\begin{pmatrix}
+              ${a3.map((v) => this.complexToStr(v)).join('\\\\')}
+            \\end{pmatrix}
+          \\end{pmatrix}
+          =\\begin{pmatrix}
+            ${a4.map((v) => this.complexToStr(v)).join('\\\\')}
           \\end{pmatrix}
         \\end{aligned}
-      `;
-      this.expressions.push(genF(pol1Rev));
+      `,
+        };
+      };
+
+      const { result: resPol1, text: textPol1 } = genF(pol1Rev);
+      const { result: resPol2, text: textPol2 } = genF(pol2Rev);
+      this.expressions.push(textPol1);
+      this.expressions.push(textPol2);
+
+      this.expressions.push(
+        `(${resPol1.map((v) => this.complexToStr(v)).join(',')}) \\otimes (${resPol2
+          .map((v) => this.complexToStr(v))
+          .join(',')})`
+      );
+
+      const polMul = resPol1.map((v, i) => this.mulComplex(v, resPol2[i]));
+      this.expressions.push(
+        `\\text{Multiplicar Vetores: }= (${polMul.map((v) => this.complexToStr(v)).join(',')})`
+      );
+
+      const polCompl = polMul.map((v) => this.complementComplex(v));
+      this.expressions.push(
+        `\\text{Conjugar} \\rightarrow (${polCompl.map((v) => this.complexToStr(v)).join(',')})`
+      );
+
+      const polComplRev2 = this.rev2(polCompl);
+      this.expressions.push(
+        `\\text{Rev}_2\\rightarrow (${polComplRev2.map((v) => this.complexToStr(v)).join(',')})`
+      );
+
+      const { result: resPol3, text: textPol3 } = genF(polComplRev2);
+      this.expressions.push(textPol3);
+
+      const resDivided = resPol3.map((v) =>
+        this.mulComplex(this.complementComplex(v), { real: 0.25, imag: 0 })
+      );
+      this.expressions.push(
+        `\\text{Conjungar e dividir por }2^2: (${resPol3
+          .map((v) => this.complexToStr(v))
+          .join(',')}) / 4 = (${resDivided.map((v) => this.complexToStr(v)).join(',')})`
+      );
+
+      this.expressions.push(`\\text{Resposta: }\\begin{array}{llll}
+        ${resDivided.map((v, i) => `${this.complexToStr(v)}n^${i}`).join('&')}
+      \\end{array}`);
     },
   },
 };
