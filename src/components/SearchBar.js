0ab932ddb9b6c30d0919ffbb7dec270dea0d6f8c
Diogo Correia <me@diogotc.com>
2021-07-16 19:15:27 +0100

    Fix webpack build issues with searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,22 +1,29 @@
-import 'docs-searchbar.js/dist/cdn/docs-searchbar.css';
-import docsSearchBar from 'docs-searchbar.js/dist/cdn/docs-searchbar.js';
 import { navigate } from 'gatsby-link';
 import React, { useEffect } from 'react';
 import '../styles/searchbar.css';
 
 const SearchBar = () => {
   useEffect(() => {
-    docsSearchBar({
-      hostUrl: 'https://meilisearch.diogotc.com/',
-      apiKey: 'c2972e080f75e3d6891861c6a06ab5e335ccf16c395dcba2322eab5027cba783',
-      indexUid: 'resumos-leic-v2',
-      enableDarkMode: true,
-      inputSelector: '#searchbar-input',
-      handleSelected: (_input, _event, suggestion) => {
-        const url = new URL(suggestion.url);
-        navigate(url.href.replace(url.origin, ''));
-      },
-    });
+    Promise.all([
+      import(
+        /* webpackChunkName: "docs-searchbar" */ 'docs-searchbar.js/dist/cdn/docs-searchbar.min.js'
+      ),
+      import(
+        /* webpackChunkName: "docs-searchbar" */ 'docs-searchbar.js/dist/cdn/docs-searchbar.min.css'
+      ),
+    ]).then(([docsSearchBar]) =>
+      docsSearchBar.default({
+        hostUrl: 'https://meilisearch.diogotc.com/',
+        apiKey: 'c2972e080f75e3d6891861c6a06ab5e335ccf16c395dcba2322eab5027cba783',
+        indexUid: 'resumos-leic-v2',
+        enableDarkMode: true,
+        inputSelector: '#searchbar-input',
+        handleSelected: (_input, _event, suggestion) => {
+          const url = new URL(suggestion.url);
+          navigate(url.href.replace(url.origin, ''));
+        },
+      })
+    );
   }, []);
 
   return (
