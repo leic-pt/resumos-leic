c6a4781b432f355ae3b7ce9b77b53b34f9a9c2d1
Diogo Correia <me@diogotc.com>
2022-05-09 17:11:02 +0100

    Fix wrong class

--- src/components/SearchBar/NoResults.js
+++ src/components/SearchBar/NoResults.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const NoResults = ({ query }) => {
   return (
-    <div class='search-no-results'>
-      <div class='search-no-results--icon'>(;-;)</div>
-      <div class='search-no-results--text'>{`No results found for '${query}'`}</div>
+    <div className='search-no-results'>
+      <div className='search-no-results--icon'>(;-;)</div>
+      <div className='search-no-results--text'>{`No results found for '${query}'`}</div>
     </div>
   );
 };
