ea8e6cbbb6bc91c4b80e1e6723eca37a1ccd1b0f
Diogo Correia <me@diogotc.com>
2022-05-09 16:23:50 +0100

    Improve start search menu with course list

--- src/components/SearchBar/StartSearching.js
+++ src/components/SearchBar/StartSearching.js
@@ -1,9 +1,50 @@
+import { graphql, Link, useStaticQuery } from 'gatsby';
 import React from 'react';
 
 const StartSearching = () => {
+  const { markdownRemark: homepage } = useStaticQuery(graphql`
+    query SearchStartQuery {
+      markdownRemark(frontmatter: { path: { eq: "/" } }) {
+        frontmatter {
+          years {
+            name
+            semesters {
+              name
+              courses {
+                name
+                link
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  const { years } = homepage.frontmatter;
+
   return (
     <div className='search-start'>
-      <div className='search-start--heading'>Results will show up here</div>
+      {years.map((year) => (
+        <div key={year.name} className='search-start-group'>
+          <p className='search-start-group--name'>{year.name}</p>
+          <div className='search-start-group-columns'>
+            {year.semesters.map((semester) => (
+              <div key={semester.name} className='search-start-group-column'>
+                {semester.courses.map((course) => (
+                  <Link
+                    to={course.link}
+                    key={course.link}
+                    className='search-start-group-column--item'
+                  >
+                    {course.name}
+                  </Link>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
