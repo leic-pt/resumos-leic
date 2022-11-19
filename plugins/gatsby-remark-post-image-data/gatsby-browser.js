93fc956410f6bd5ebe3487f0f9a4886a0e59508d
Diogo Correia <me@diogotc.com>
2022-11-19 13:01:40 +0000

    Fix text not showing up correctly in complex SVGs in Chrome

---
+++ plugins/gatsby-remark-post-image-data/gatsby-browser.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll(`object[type="image/svg+xml"]`).forEach(swapObjectByInlineSvg);
+});
+
+/**
+ * Chromium did not want to correctly load MathJax inside the draw.io SVGs
+ * when they were wrapped in an <object> tag.
+ * This function takes an <object> element, gets its content and places the SVG inline,
+ * avoiding the <object> tag.
+ */
+const swapObjectByInlineSvg = async (el) => {
+  let svgElement = el.contentDocument?.querySelector('svg');
+  if (!svgElement) {
+    // Don't do anything if there is no SVG element
+    return;
+  }
+
+  const svgSource = svgElement.outerHTML;
+
+  let divWrapper = document.createElement('div');
+  divWrapper.innerHTML = svgSource;
+
+  // Copy attributes from old element to new
+  el.getAttributeNames()
+    .filter((attr) => ['data', 'type', 'onload'].indexOf(attr) === -1) // remove stale attributes
+    .forEach((attr) => {
+      divWrapper.setAttribute(attr, el.getAttribute(attr));
+    });
+
+  divWrapper.classList.add('object-svg-inline');
+
+  el.parentElement.insertBefore(divWrapper, el);
+  el.parentElement.removeChild(el);
+};
+
+window.swapObjectByInlineSvg = swapObjectByInlineSvg;
