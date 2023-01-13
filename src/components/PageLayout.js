ae591e27bde8e7400732d410ba240988c2e6ebbe
Francisco Salgueiro <92053465+franciscoBSalgueiro@users.noreply.github.com>
2023-01-13 23:51:15 +0000

    Add edit button (#906)
    Co-authored-by: Diogo Correia <me@diogotc.com>

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -6,6 +6,7 @@ import '../styles/main.css';
 import '../styles/markdown.css';
 import { customComponents } from '../utils/customComponents';
 import Footer from './Footer';
+import Edit from './icons/Edit';
 import Navbar from './Navbar';
 import PageMetadata from './PageMetadata';
 import Sidebar from './Sidebar';
@@ -25,6 +26,11 @@ export default function Template({ data }) {
     (page) => page.node.childMarkdownRemark.frontmatter.type === 'topLevelPage'
   )?.node.childMarkdownRemark.frontmatter.title;
 
+  const { relativePath } = page.parent;
+  const githubLink = relativePath
+    ? `${data.site.siteMetadata.footer.githubLink}/edit/master/content/${relativePath}`
+    : null;
+
   return (
     <CurrentSectionProvider value={currentSection}>
       <div className={`page-container ${sidebarOpen ? `sidebar-open` : ``}`}>
@@ -33,6 +39,11 @@ export default function Template({ data }) {
         <Navbar toggleSidebar={toggleSidebar} />
         <Sidebar paths={sidebarPaths} sidebarOpen={sidebarOpen} toggleSidebar={toggleSidebar} />
         <div className='main-container'>
+          {githubLink && (
+            <a href={githubLink} className='edit-link'>
+              <Edit /> Edit page
+            </a>
+          )}
           <div className='content' dangerouslySetInnerHTML={{ __html: page.html }} />
           {components?.map((Component, i) => (
             <Component key={i} />
@@ -54,6 +65,11 @@ export const pageQuery = graphql`
         description
         components
       }
+      parent {
+        ... on File {
+          relativePath
+        }
+      }
     }
     allFile(
       filter: {
@@ -71,6 +87,13 @@ export const pageQuery = graphql`
               type
             }
           }
+        }
+      }
+    }
+    site {
+      siteMetadata {
+        footer {
+          githubLink
         }
       }
     }
