86c24264e52db3c6711fc42faeafbbba6324e217
Diogo Correia <me@diogotc.com>
2023-02-05 20:35:53 +0000

    Add sidepanel component

---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { useTrapFocus } from '../../hooks/useTrapFocus';
+import useViewHeightCSS from '../../hooks/useViewHeightCSS';
+import './SidePanel.css';
+
+const BODY_SIDEPANEL_OPEN_CLASSNAME = 'body--sidepanel-open';
+
+const Dialog = ({ open, onClose, children }) => {
+  useEffect(() => {
+    if (open) {
+      document.body.classList.add(BODY_SIDEPANEL_OPEN_CLASSNAME);
+    } else {
+      document.body.classList.remove(BODY_SIDEPANEL_OPEN_CLASSNAME);
+    }
+
+    return () => {
+      document.body.classList.remove(BODY_SIDEPANEL_OPEN_CLASSNAME);
+    };
+  }, [open]);
+
+  const containerRef = React.useRef(null);
+  useTrapFocus({ container: containerRef.current });
+  useViewHeightCSS(open);
+
+  return createPortal(
+    <div className={`sidepanel-container${open ? ` sidepanel-container--open` : ``}`}>
+      <div
+        className='sidepanel-backdrop'
+        ref={containerRef}
+        role='button'
+        tabIndex={0}
+        onMouseDown={(event) => {
+          if (event.target === event.currentTarget) {
+            onClose();
+          }
+        }}
+      />
+      <div className='sidepanel-modal'>{children}</div>
+    </div>,
+    document.body
+  );
+};
+
+export default Dialog;
--- src/components/ThemeSettings.js
+++ src/components/ThemeSettings.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDarkMode, useFontSettings, useThemeSettings } from '../hooks/theme-hooks';
 import DarkModeOff from './icons/DarkModeOff';
 import DarkModeOn from './icons/DarkModeOn';
+import SidePanel from './SidePanel';
 
 const ThemeSettings = () => {
+  const [panelOpen, setPanelOpen] = useState(false);
+  const closePanel = useCallback(() => setPanelOpen(false), [setPanelOpen]);
+  const togglePanel = useCallback(() => setPanelOpen((v) => !v), [setPanelOpen]);
   const { darkMode, toggle: toggleDarkMode } = useDarkMode();
   useFontSettings();
   useThemeSettings();
 
   return (
-    <button className='dark-mode-toggle' onClick={toggleDarkMode}>
-      {darkMode ? <DarkModeOn /> : <DarkModeOff />}
-    </button>
+    <>
+      <button className='dark-mode-toggle' onClick={togglePanel}>
+        {darkMode ? <DarkModeOn /> : <DarkModeOff />}
+      </button>
+      <SidePanel open={panelOpen} onClose={closePanel}>
+        <p>Reading Options</p>
+        <hr />
+        Test
+      </SidePanel>
+    </>
   );
 };
 
