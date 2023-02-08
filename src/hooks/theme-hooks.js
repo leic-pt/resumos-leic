2e56ef7948bac5fad9260eca37e48524656b191e
Diogo Correia <me@diogotc.com>
2023-02-08 15:34:36 +0000

    Add sidepanel theme settings (#940)

--- src/hooks/theme-hooks.js
+++ src/hooks/theme-hooks.js
@@ -1,22 +1,104 @@
-import { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import '../styles/themes/black.css';
 import '../styles/themes/nord.css';
 import '../styles/themes/solarized.css';
 
-// Custom hook
+const fonts = {
+  roboto: {
+    cssFamily: 'Roboto, sans-serif',
+    url: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
+  },
+  comicNeue: {
+    cssFamily: 'Comic Neue, sans-serif',
+    url: 'https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&display=swap',
+  },
+  indieFlower: {
+    cssFamily: 'Indie Flower, cursive',
+    url: 'https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap',
+  },
+  nunito: {
+    cssFamily: 'Nunito, sans-serif',
+    url: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap',
+  },
+  openDyslexic: {
+    cssFamily: 'OpenDyslexicRegular, Roboto, sans-serif',
+    url: 'https://cdn.jsdelivr.net/npm/open-dyslexic@1.0.3/open-dyslexic-regular.min.css',
+  },
+  openSans: {
+    cssFamily: 'Open Sans, Roboto, sans-serif',
+    url: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap',
+  },
+  cursive: {
+    cssFamily: 'cursive',
+  },
+  monospace: {
+    cssFamily: 'monospace',
+  },
+  sansSerif: {
+    cssFamily: 'sans-serif',
+  },
+  serif: {
+    cssFamily: 'serif',
+  },
+};
+
 export function useFontSettings() {
-  const [font, setFont] = useLocalStorage('customFont');
+  const [font, setFont] = useLocalStorage('customFont', 'roboto');
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
 
     const element = window.document.body;
-    element.style.fontFamily = font || null;
-  }, [font, setFont]);
+    const selectedFont = fonts[font] || fonts.roboto;
+    element.style.fontFamily = selectedFont.cssFamily;
+  }, [font]);
+
+  const fontLoader = useMemo(() => {
+    const selectedFont = fonts[font] || fonts.roboto;
+    if (!selectedFont.url) return null;
+
+    return <link href={selectedFont.url} rel='stylesheet'></link>;
+  }, [font]);
+
+  return { fontLoader, font, setFont };
+}
+
+export function useContentWidth() {
+  const [contentWidth, setContentWidth] = useLocalStorage('contentWidth', 'compact');
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const element = window.document.body;
+    if (contentWidth === 'full') {
+      element.classList.add('full-width');
+    } else {
+      element.classList.remove('full-width');
+    }
+  });
+
+  return { contentWidth, setContentWidth };
+}
+
+export function useTextAlign() {
+  const [textAlign, setTextAlign] = useLocalStorage('textAlign', 'left');
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+
+    const element = window.document.body;
+    if (textAlign === 'justify') {
+      element.classList.add('text-justify');
+    } else {
+      element.classList.remove('text-justify');
+    }
+  });
+
+  return { textAlign, setTextAlign };
 }
 
 export function useThemeSettings() {
-  const [theme, setTheme] = useLocalStorage('themeName');
+  const [theme, setTheme] = useLocalStorage('themeName', 'default');
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
@@ -50,9 +132,7 @@ export function useDarkMode() {
     }
   }, [darkMode]);
 
-  const toggle = useCallback(() => setDarkModeStored(!darkMode), [setDarkModeStored, darkMode]);
-
-  return { darkMode, toggle };
+  return { darkMode: darkModeStored, setDarkModeStored };
 }
 
 // Source: https://usehooks.com/useLocalStorage/
