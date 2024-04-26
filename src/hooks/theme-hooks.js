75396947cc8656823111170984d103f38277e72a
Dinis Esteves <66919841+Dinis-Esteves@users.noreply.github.com>
2024-04-26 15:39:29 +0100

    feat: display each font text with the corresponding font in theme settings (#1023)
    Co-authored-by: Diogo Correia <me@diogotc.com>

--- src/hooks/theme-hooks.js
+++ src/hooks/theme-hooks.js
@@ -4,42 +4,52 @@ import '../styles/themes/gruvbox.css';
 import '../styles/themes/nord.css';
 import '../styles/themes/solarized.css';
 
-const fonts = {
+export const fonts = {
   roboto: {
     cssFamily: 'Roboto, sans-serif',
     url: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap',
+    displayName: 'Roboto (default)',
   },
   comicNeue: {
     cssFamily: 'Comic Neue, sans-serif',
     url: 'https://fonts.googleapis.com/css2?family=Comic+Neue:wght@400;700&display=swap',
+    displayName: 'Comic Neue',
   },
   indieFlower: {
     cssFamily: 'Indie Flower, cursive',
     url: 'https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap',
+    displayName: 'Indie Flower',
   },
   nunito: {
     cssFamily: 'Nunito, sans-serif',
     url: 'https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap',
+    displayName: 'Nunito',
   },
   openDyslexic: {
     cssFamily: 'OpenDyslexicRegular, Roboto, sans-serif',
     url: 'https://cdn.jsdelivr.net/npm/open-dyslexic@1.0.3/open-dyslexic-regular.min.css',
+    displayName: 'OpenDyslexic',
   },
   openSans: {
     cssFamily: 'Open Sans, Roboto, sans-serif',
     url: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap',
+    displayName: 'Open Sans',
   },
   cursive: {
     cssFamily: 'cursive',
+    displayName: 'cursive (system)',
   },
   monospace: {
     cssFamily: 'monospace',
+    displayName: 'monospace (system)',
   },
   sansSerif: {
     cssFamily: 'sans-serif',
+    displayName: 'sans-serif (system)',
   },
   serif: {
     cssFamily: 'serif',
+    displayName: 'serif (system)',
   },
 };
 
