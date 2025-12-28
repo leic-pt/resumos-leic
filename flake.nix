6d670afaac32f2d7c2bd40e22a75ff50332a9868
Diogo Correia <me@diogotc.com>
2025-12-28 14:23:35 +0000

    chore: add shell.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            nodejs
-            yarn
-          ];
-        };
-      }
-    );
+  outputs =
+    { nixpkgs, ... }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs [
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+    in
+    {
+      devShell = eachSystem (
+        system:
+        import ./shell.nix {
+          pkgs = import nixpkgs { inherit system; };
+        }
+      );
+    };
 }
